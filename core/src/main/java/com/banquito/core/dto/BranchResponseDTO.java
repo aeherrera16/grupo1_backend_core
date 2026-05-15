a10b0797936a93afd14d4f7ea53149457f50d8ed
Oscarf05 <ofchanataxi@espe.edu.ec>
2026-05-15 13:29:32 -0500

    Enviar fecha de creacion de la sucursal

--- core/src/main/java/com/banquito/core/dto/BranchResponseDTO.java
+++ core/src/main/java/com/banquito/core/dto/BranchResponseDTO.java
@@ -2,15 +2,16 @@ package com.banquito.core.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import java.time.LocalDateTime;
 
 @Getter
-@Setter
 public class BranchResponseDTO {
 
     private Integer id;
     private String branchCode;
     private String name;
     private String city;
+    private LocalDateTime creationDate;
 
     public BranchResponseDTO() {
     }
@@ -20,5 +21,6 @@ public class BranchResponseDTO {
         this.branchCode = branchCode;
         this.name = name;
         this.city = city;
+        this.creationDate = creationDate;
     }
 }
